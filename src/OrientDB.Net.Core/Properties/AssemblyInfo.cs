5a39d5e9346299a2904368f35fdd69fc81a0cbb1
Nezumi-Quasar <Nezumi-Quasar@users.noreply.github.com>
2017-09-14 14:15:16 +0200

    Changed version in order to publish on nuget

--- src/OrientDB.Net.Core/Properties/AssemblyInfo.cs
+++ src/OrientDB.Net.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Orient Technologies")]
+[assembly: AssemblyCompany("Orient ltd")]
 [assembly: AssemblyProduct("OrientDB.Net.Core")]
 [assembly: AssemblyTrademark("")]
 
